33375d8f729c924f76fa2b9d793369664dde0001
Harvey Ngo <harveydngo@gmail.com>
2015-07-14 00:17:48 -0700

    add hex gallery

--- app/controllers/photo.rb
+++ app/controllers/photo.rb
@@ -16,3 +16,16 @@ get '/photo/:id' do
 	erb :"photo/show"
 end
 
+get '/photo/:id/edit' do
+	@photo = Photo.find_by(id: params[:id])
+	@user = Bumblebee.find_by(id: @photo.bumblebee_id)
+	erb :"photo/edit"
+end
+
+put '/photo/:id/edit' do
+	@photo = Photo.find_by(id: params[:id])
+	@photo.update_attributes(location: params[:location], link: @photo.link, caption: params[:caption], bumblebee_id: current_user.id)
+	@photo.save!
+	redirect "/photo/#{@photo.id}"
+end
+
