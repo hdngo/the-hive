32af8e9f2dc21bc7b274c12c9c0f841fe777b0e3
Harvey Ngo <harveydngo@gmail.com>
2015-07-18 17:58:32 -0700

    add more css and change up some html elements

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,4 @@
 class Friendship < ActiveRecord::Base
-  # Remember to create a migration!
-  belongs_to :bumblebee
-  belongs_to :friend, class_name: "Bumblebee"
+  belongs_to :bumblebee, :class_name => "Bumblebee"
+  belongs_to :friend, :class_name => "Bumblebee"
 end
--- db/migrate/20150715161237_create_messages.rb
+++ db/migrate/20150715161237_create_messages.rb
@@ -1,12 +1,13 @@
 class CreateMessages < ActiveRecord::Migration
   def change
-  	create_table :messages do |t|
-  		# t.integer :sender_id
-  		t.integer :recipient_id
-  		t.text :content
+  	create_table   :messages do |t|
+  		# t.integer  :sender_id
+  		t.integer    :recipient_id
+  		t.text       :content
 
   		t.timestamps
-  		t.references :bumblebee, index: true
+  		t.references :bumblebee, 
+  								 :index => true
   	end
   end
 end
